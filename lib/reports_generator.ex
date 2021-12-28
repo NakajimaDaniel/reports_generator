3dc7da2345280769fe0b0eb3d12723608ee7e664
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-12-28 19:57:35 -0300

    feat: using pipe operator and reading file

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,8 +1,19 @@
 defmodule ReportsGenerator do
   def build(filename) do
-    case File.read("reports/#{filename}") do
-      {:ok, result} -> result
-      {:error, error} -> error
-    end
+    # case File.read("reports/#{filename}") do
+    #   {:ok, result} -> result
+    #   {:error, error} -> error
+    # end
+
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
   end
 end
