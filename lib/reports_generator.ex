5fe1f8ab298f7994e62da52acc775332ebb2cb7b
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-12-29 19:36:37 -0300

    feat: sum all orders

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -7,7 +7,10 @@ defmodule ReportsGenerator do
 
     "reports/#{filename}"
     |> File.stream!()
-    |> Enum.map(fn line -> parse_line(line) end)
+    |> Enum.reduce(report_acc(), fn line, report ->
+      [id, _food_name, price] = parse_line(line)
+      Map.put(report, id, price)
+    end)
   end
 
   defp parse_line(line) do
@@ -16,4 +19,6 @@ defmodule ReportsGenerator do
     |> String.split(",")
     |> List.update_at(2, &String.to_integer/1)
   end
+
+  defp report_acc, do: Enum.into(1..30, %{}, fn x -> {Integer.to_string(x), 0} end)
 end
