e18112169b5dc7067c007f57d4c567f2cd96df04
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-12-30 18:32:53 -0300

    feat: return the most consumed item

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -12,13 +12,22 @@ defmodule ReportsGenerator do
     "sushi"
   ]
 
+  @options [
+    "foods",
+    "users"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "invalid option"}
 
   defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
     users = Map.put(users, id, users[id] + price)
