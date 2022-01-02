cb9d7b007c23ff1d5175778d5ff920bcae1ff43f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-01-02 19:24:27 -0300

    feat: build report with parallelism

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -23,27 +23,48 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(fn filename -> build(filename) end)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
+         "foods" => foods2,
+         "users" => users2
+       }) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
   def fetch_higher_cost(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
 
   def fetch_higher_cost(_report, _option), do: {:error, "invalid option"}
 
-  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods}) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
-    report
-    |> Map.put("users", users)
-    |> Map.put("foods", foods)
+    # report
+    # |> Map.put("users", users)
+    # |> Map.put("foods", foods)
 
-    # %{report | "users" => users, "foods" => foods}
+    build_report(foods, users)
   end
 
   defp report_acc do
     users = Enum.into(1..30, %{}, fn x -> {Integer.to_string(x), 0} end)
     foods = Enum.into(@available_foods, %{}, fn x -> {x, 0} end)
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
